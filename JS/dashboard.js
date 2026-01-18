1f89625c9f9c0a3656f141ec9acce2c4e6d2b39a
Immanuel <emmanuelmendoza448@gmail.com>
2026-01-18 12:52:58 -0600

    se le da fucnionamiento al botón salir del dashboard

--- JS/dashboard.js
+++ JS/dashboard.js
@@ -1,25 +1,40 @@
 // =====================
 // CHART: USUARIOS (dashboard)
 // =====================
-const ctxUsers = document.getElementById('usersChart');
+const ctxUsers = document.getElementById("usersChart");
 
 if (ctxUsers) {
   new Chart(ctxUsers, {
-    type: 'line',
+    type: "line",
     data: {
-      labels: ['Ene','Feb','Mar','Abr','May','Jun'],
-      datasets: [{
-        label: 'Usuarios',
-        data: [120, 190, 300, 250, 420, 380],
-        borderWidth: 2,
-        tension: 0.4
-      }]
+      labels: ["Ene", "Feb", "Mar", "Abr", "May", "Jun"],
+      datasets: [
+        {
+          label: "Usuarios",
+          data: [120, 190, 300, 250, 420, 380],
+          borderWidth: 2,
+          tension: 0.4,
+        },
+      ],
     },
     options: {
       responsive: true,
       plugins: {
-        legend: { display: false }
-      }
-    }
+        legend: { display: false },
+      },
+    },
   });
 }
+
+// === BOTÓN SALIR ===
+const btnLogout = document.getElementById("btnLogout");
+
+btnLogout.addEventListener("click", () => {
+  const confirmacion = confirm("¿Deseas salir del panel?");
+
+  if (confirmacion) {
+    window.location.href = "index.html";
+  }
+});
+
+
