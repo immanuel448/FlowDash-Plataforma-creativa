4d82d090f827f089cd3725b0bcf7acb92acbbbd1
Immanuel <emmanuelmendoza448@gmail.com>
2026-01-18 13:22:06 -0600

    agregar creación dinámica de usuarios

--- JS/modal.js
+++ JS/modal.js
@@ -1,12 +1,40 @@
-// === MODAL NUEVO USUARIO ===
+// === MODAL USUARIOS ===
+
 const btnNuevo = document.getElementById("btnNuevoUsuario");
 const modal = document.getElementById("modalUsuario");
 const cerrar = document.getElementById("cerrarModal");
+const btnGuardar = document.getElementById("btnGuardar");
 
-btnNuevo.addEventListener("click",()=>{
-  modal.style.display="flex";
-});
+btnNuevo.onclick = ()=> modal.style.display="flex";
+cerrar.onclick = ()=> modal.style.display="none";
 
-cerrar.addEventListener("click",()=>{
+// GUARDAR
+btnGuardar.onclick = ()=>{
+
+  let nombre = document.getElementById("nombre").value;
+  let correo = document.getElementById("correo").value;
+  let rol = document.getElementById("rol").value;
+
+  if(!nombre || !correo){
+    alert("Completa los campos");
+    return;
+  }
+
+  let fila = `
+    <tr>
+      <td>${nombre}</td>
+      <td>${correo}</td>
+      <td>${rol}</td>
+    </tr>
+  `;
+
+  document.getElementById("tablaUsuarios")
+  .insertAdjacentHTML("beforeend",fila);
+
   modal.style.display="none";
-});
+
+  // limpiar
+  nombre.value="";
+  correo.value="";
+}
+
