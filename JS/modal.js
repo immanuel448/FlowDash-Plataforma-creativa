b17b8f961a8059a0b6ddeefb2214463ccf5eb93e
Immanuel <emmanuelmendoza448@gmail.com>
2026-01-18 19:37:33 -0600

    eliminar usuarios desde tabla

--- JS/modal.js
+++ JS/modal.js
@@ -3,30 +3,41 @@ const modal = document.getElementById("modalUsuario");
 const btnNuevo = document.getElementById("btnNuevoUsuario");
 const cerrar = document.getElementById("cerrarModal");
 
-btnNuevo.onclick = () => modal.style.display = "flex";
-cerrar.onclick = () => modal.style.display = "none";
-
+btnNuevo.onclick = () => (modal.style.display = "flex");
+cerrar.onclick = () => (modal.style.display = "none");
 
 // === DATOS ===
 let usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
 
-
 // === PINTAR TABLA ===
-function renderTabla(){
+function renderTabla() {
   const tbody = document.getElementById("tablaUsuarios");
   tbody.innerHTML = "";
 
-  usuarios.forEach(u => {
+  usuarios.forEach((u, index) => {
     tbody.innerHTML += `
       <tr>
         <td>${u.nombre}</td>
         <td>${u.correo}</td>
         <td>${u.rol}</td>
+        <td>
+          <button class="btn-delete" onclick="eliminarUsuario(${index})">
+            Eliminar
+          </button>
+        </td>
       </tr>
     `;
   });
 }
 
+// === ELIMINAR ===
+function eliminarUsuario(i) {
+  if (!confirm("¿Eliminar usuario?")) return;
+
+  usuarios.splice(i, 1);
+  localStorage.setItem("usuarios", JSON.stringify(usuarios));
+  renderTabla();
+}
 
 // === GUARDAR ===
 document.getElementById("btnGuardar").onclick = () => {
@@ -34,7 +45,7 @@ document.getElementById("btnGuardar").onclick = () => {
   const correo = document.getElementById("correo").value;
   const rol = document.getElementById("rol").value;
 
-  if(!nombre || !correo) return alert("Completa los campos");
+  if (!nombre || !correo) return alert("Completa los campos");
 
   usuarios.push({ nombre, correo, rol });
   localStorage.setItem("usuarios", JSON.stringify(usuarios));
@@ -46,7 +57,6 @@ document.getElementById("btnGuardar").onclick = () => {
   nombre.value = "";
   correo.value = "";
 };
-
 
 // inicial
 renderTabla();
